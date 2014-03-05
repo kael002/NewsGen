91130ef65733fcf72034b382de0b12d37a1232eb
kael002 <kael002@gmail.com>
2014-03-05 14:04:21 -0600

    Moved stuff to global and continued with LB
    * Moved all general helpers and shims to global.js
    * Added timer to set up intervals (decided to use requestAnimationFrame
    instead of setInterval.. mainly because I wanted to learn what it can
    do)
    * added html framework

--- includes/script/global.js
+++ includes/script/global.js
@@ -5,14 +5,54 @@
  * Time: 2:02 PM
  * To change this template use File | Settings | File Templates.
  */
+"use strict";
+(function(BFG){
 
-(function(){
-	"use strict";
 	var initializing = false,
 		superPattern = /xyz/.test(function(){xyz;})?/\b_super\b/:/.*/;
 
-})();
-function assert(value,message){
+	BFG.extend = function(obj){
+		var 	key,
+			args = Array.prototype.slice.call(arguments,1);
+		args.forEach(function(value,index,array){
+			for (key in value){
+				if (value.hasOwnProperty(key)){
+					obj[key] = value[key];
+				}
+			}
+		});
+		return obj;
+	};
+	window.BFG = BFG;
+})(window.BFG || {});
+
+//Erik Möller - polyfill for requestAnimationFrame
+(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+window.assert = window.assert || function (value,message){
 	if(!message){
 		message = value;
 		value = true;
@@ -27,4 +67,10 @@ function assert(value,message){
 	li.className = value ? 'success':'fail';
 	li.appendChild(document.createTextNode(message));
 	ul.appendChild(li);
-}
+};
+
+Array.prototype.forEach = Array.prototype.forEach || function(callback,context){
+	for(var i = 0,len = this.length;i < len;i++){
+		callback.call(context || null,this[i],i,this);
+	}
+};
--- includes/script/leaderboard.js
+++ includes/script/leaderboard.js
@@ -5,92 +5,73 @@
 // allow for custom click/hover events
 "use strict";
 (function(BFG){
-	BFG.extend = function(obj){ // I should really put this in a global.js file
-		var args = Array.prototype.slice.call(arguments,1);
-		args.forEach(function(value,index,array){
-			var key ;
-			for (key in value){
-				if (value.hasOwnProperty(key)){
-					obj[key] = value[key];
-				}
-			}
-		});
-		return obj;
-	};
 
 	var Leaderboard = function(o){
 		this.config = BFG.extend({
 			maxDisplay:10,
 			interval:10,
-			hover:function(){
-				return;
-			},
-			transition:function(){
-				return;
-			}
+			elemId:'leaderboard'
 		},o);
 		this.data = [];
 		this.uiList = [];
+		this.animationRequestId = 0;
 	};
-	BFG.extend(Leaderboard.prototype,{
+	Leaderboard.prototype = {
+		constructor:Leaderboard,
 		start:function(){
-
-			console.log('hit');
-			//requestAnimationFrame( this.run );
+			var 	that = this,
+				tStart = 0,
+				progress = 0,
+				interval = that.config.interval * 1000,
+				doBuildUI = this.uiList.length === 0;
+			if (doBuildUI){
+				this.buildUI();
+			}
+			(function run(timestamp){ //This should be moved into it's own .. passing in something like {startTime:0,interval:1000,callback:fn,endOnly:true}
+				progress = timestamp - tStart ;
+				if (progress > interval || progress === 0){
+					tStart = timestamp;
+					that.getData();
+					that.doTransition();
+				}
+				requestAnimationFrame( run );
+			})(0);
 		},
 		stop:function(){
-			clearInterval(this.timer);
+			//This no worky
+			console.log(this.animationRequestId);
+			cancelAnimationFrame(0);
 		},
 		getData:function(){
 			console.log('getData');
 			return this;
 		},
 		buildUI:function(){
-			console.log('build UI');
+			var 	elem = document.getElementById(this.config.elemId) || document.body.appendChild(document.createElement("div")),
+				ul = elem.appendChild(document.createElement("ul"));
+			for (var i = 0;i < this.config.maxDisplay;i++){
+				this.uiList.push({
+					elem:ul.appendChild(document.createElement("li")),
+					targetX:0,
+					targetY:0
+				});
+				this.uiList[i].elem.innerHTML = "Loading...";
+			}
 			return this;
 		},
 		doTransition:function(){
 			console.log('doTransision');
 			return this;
 		}
-	});
+	};
 
 	BFG.Leaderboard = Leaderboard;
 
-	Array.prototype.forEach = Array.prototype.forEach || function(callback,context){
-		for(var i = 0,len = this.length;i < len;i++){
-			callback.call(context || null,this[i],i,this);
-		}
-	};
+
 	window.BFG = BFG;
 })(window.BFG || {});
 
-//Erik Möller - polyfill for requestAnimationFrame
-(function() {
-    var lastTime = 0;
-    var vendors = ['webkit', 'moz'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame =
-          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
 
 var test = new BFG.Leaderboard({interval:20});
-console.log(test);
 test.start();
